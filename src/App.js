f526a794336e3d504f00220840d577c787ab476e
gaorock530 <gaorock530@hotmail.com>
2019-08-20 13:41:10 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header';
+import Dangan from './components/sec_dangan';
+import Jishi from './components/sec_jishi';
+import Jianyi from './components/sec_jianyi';
+import About from './components/sec_about';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <div className="placeholder"></div>
+      <section className="sec-bg" id="start"></section>
+      <Dangan/>
+      <section className="sec-dangan">
+        <div className="container">
+          <div className="left"></div>
+          <div className="right"></div>
+        </div>
+      </section>
+      <Jishi/>
+      <section className="sec-jishi">
+        <div className="container">
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          牧葵独有的特殊工艺，最大限度的锁住营养成分，每一口咬下去，都可以感受到参肉有力道回馈于牙齿的弹力，不用费力就可以嚼烂，但是绝不会轻易嚼碎，区别于市面上所有海参的筋道口感称为“牧葵筋”。          </p>
+        </div>
+      </section>
+      <Jianyi/>
+      <About/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="footer">
+      <div className="contact">
+        <a className="c1" href="https://shop363159177.taobao.com/" rel="noopener noreferrer" target="_blank"><div></div></a>
+        <div className="c2"></div>
+        <div className="c3"></div>
+        <div className="c4"></div>
+      </div>
+      <h3>400 - 0373 - 909</h3>
+      <p>河南省内免费配送  Copyright © 2019 Mukui. 保留所有权利。</p>
+    </footer>
+  )
+}
