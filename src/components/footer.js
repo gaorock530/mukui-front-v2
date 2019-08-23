4eda44a40396e98563095c0e3c41c96a7b8f95b1
gaorock530 <gaorock530@hotmail.com>
2019-08-23 16:01:53 +0800

    add QC

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,55 @@
 import React from 'react';
 
-export default () => {
-  return (
-    <footer className="footer">
-      <div className="contact">
-        <a className="c1" href="https://shop363159177.taobao.com/" rel="noopener noreferrer" target="_blank"><div></div></a>
-        <div className="c2"></div>
-        <a className="c3" href="https://weibo.com/470038388" rel="noopener noreferrer" target="_blank"><div></div></a>
-        <div className="c4"></div>
-      </div>
-      <h3>400 - 0373 - 909</h3>
-      <p>河南省内免费配送  Copyright © 2019 Mukui. 保留所有权利。</p>
-    </footer>
-  )
+export default class Footer extends React.PureComponent {
+
+  //  src="assets/utils/douyin.png" alt="douyin"
+  constructor (props) {
+    super(props);
+    this.index = 0;
+  }
+
+  showDouyin = () => {
+    if (this.index !== 1) {
+      this.img.src = "assets/utils/douyin.png";
+      this.index = 1;
+      this.img.classList.remove('close');
+    } else {
+      this.index = 0;
+      this.img.classList.add('close');
+    }
+  }
+
+  showWeixin = () => {
+    if (this.index !== 2) {
+      this.img.src = "assets/utils/weixin.jpeg";
+      this.index = 2;
+      this.img.classList.remove('close');
+    } else {
+      this.index = 0;
+      this.img.classList.add('close');
+    }
+  }
+
+  close = () => {
+    this.index = 0;
+    this.img.classList.add('close');
+  }
+
+
+  render () {
+    return (
+      <footer className="footer">
+        <div className="contact">
+          <img ref={el => this.img = el} alt="" src="" onClick={this.close} className="close"></img>
+          <a className="c1" href="https://shop363159177.taobao.com/" rel="noopener noreferrer" target="_blank"><div></div></a>
+          <div className="c2" ref={el => this.douyin = el} onClick={this.showDouyin}></div>
+          <a className="c3" href="https://weibo.com/470038388" rel="noopener noreferrer" target="_blank"><div></div></a>
+          <div className="c4" ref={el => this.weixin = el} onClick={this.showWeixin}></div>
+        </div>
+        <h3>400 - 0373 - 909</h3>
+        <p>河南省内免费配送  Copyright © 2019 Mukui. 保留所有权利。</p>
+      </footer>
+    )
+  }
+  
 }
